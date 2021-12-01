c13c7b3affb92fac68ace57b6e4f038d7313412b
masterlg <gun.guzeloglu@gmail.com>
2021-12-01 10:43:44 +0300

    fourth commit

--- src/main/java/com/aep/training/depinj2/Depinj2Application.java
+++ src/main/java/com/aep/training/depinj2/Depinj2Application.java
@@ -1,10 +1,7 @@
 package com.aep.training.depinj2;
 
 
-import com.aep.training.depinj2.controller.ConstructorCommunicationController;
-import com.aep.training.depinj2.controller.FieldCommunicationController;
-import com.aep.training.depinj2.controller.GeneralPurposesController;
-import com.aep.training.depinj2.controller.SetterCommunicationController;
+import com.aep.training.depinj2.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,21 +12,21 @@ public class Depinj2Application {
     public static void main(String[] args) {
         /*Field Injection Örneği*/
        ApplicationContext context= SpringApplication.run(Depinj2Application.class, args);
-       FieldCommunicationController fieldCommunicationController=(FieldCommunicationController)context.getBean("fieldCommunicationController");
-       fieldCommunicationController.handleMessage();
+       //FieldCommunicationController fieldCommunicationController=(FieldCommunicationController)context.getBean("fieldCommunicationController");
+       //fieldCommunicationController.handleMessage();
 
        /*Setter Injection Örneği*/
-        //ApplicationContext context= SpringApplication.run(Depinj2Application.class, args);
-        SetterCommunicationController setterCommunicationController=(SetterCommunicationController)context.getBean("setterCommunicationController");
-        setterCommunicationController.handleMessage();
+        //SetterCommunicationController setterCommunicationController=(SetterCommunicationController)context.getBean("setterCommunicationController");
+        //setterCommunicationController.handleMessage();
 
        /*Constructor örneği*/
-       //ApplicationContext context= SpringApplication.run(Depinj2Application.class, args);
-        ConstructorCommunicationController constructorCommunicationController=(ConstructorCommunicationController)context.getBean("constructorCommunicationController");
-        constructorCommunicationController.handleMessage();
+        //ConstructorCommunicationController constructorCommunicationController=(ConstructorCommunicationController)context.getBean("constructorCommunicationController");
+        //constructorCommunicationController.handleMessage();
 
         /*GeneralPurposesController generalPurposesController =(GeneralPurposesController) context.getBean("generalPurposesController");
-        generalPurposesController.handleMessage();*/
+        //generalPurposesController.handleMessage();*/
+        UnderSamePackageLevelComponent underSamePackageLevelComponent=(UnderSamePackageLevelComponent)context.getBean("underSamePackageLevelComponent");
+        underSamePackageLevelComponent.testMe();
     }
 
 }
